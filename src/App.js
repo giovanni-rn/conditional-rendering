80f41300c15dbb2019d6797305d1420b8904c27e
oriano-dev <nono5g@protonmail.com>
2022-11-19 10:25:19 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [display, setDisplay] = useState(true);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => setDisplay(!display)}>Changer l'affichage</button>
+      {display ? <h1>Display est vrai</h1> : <h1>Display est faux</h1>}
+      {console.log(display)}
     </div>
   );
 }
